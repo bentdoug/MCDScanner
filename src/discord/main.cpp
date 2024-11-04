cd78f194fbc41957552bb72e2e9d3baebd8aff2c
Ben Douglas <bendoug3@gmail.com>
2024-11-03 20:05:25 -0500

    Init up n running

--- src/discord/main.cpp
+++ src/discord/main.cpp
@@ -6,18 +6,16 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-const std::string BOT_TOKEN = "TOKEN"; // Replace with your bot token
-const dpp::snowflake CHANNEL_ID = 1295944653966934141; // Replace with your channel ID
 
 void send_message_from_fifo(dpp::cluster& bot, dpp::snowflake channel_id) {
-    const char* fifoPath = "/Users/benjamindouglas/Documents/Personal/Projects/Radio/DiscordBots/POCSAG_bot/myfifo";
-    char buffer[256];
+    const char* fifoPath = "/home/raspi1/Documents/POCSAG_bot/montcoFifo";
+    char buffer[1200];
 
     while (true) {
         // Open the FIFO for reading
         int fd = open(fifoPath, O_RDONLY);
         if (fd < 0) {
-            std::cerr << "Failed to open FIFO for reading." << std::endl;
+            std::cerr << "Failed to open FIFO for reading: " << strerror(errno) << std::endl;
         }
 
         // Read data from the FIFO
@@ -35,25 +33,8 @@ void send_message_from_fifo(dpp::cluster& bot, dpp::snowflake channel_id) {
                     std::cerr << "Failed to send message";
                 }
             });
-        }
-        
+        }   
         close(fd);
-    }
-    while (true) {
-        // Wait for 5 minutes (300 seconds)
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-
-        // Create the message
-        dpp::message msg;
-        msg.content = "This is a periodic message!";
-        msg.channel_id = channel_id;
-
-        // Send the message
-        bot.message_create(msg, [](const dpp::confirmation_callback_t& callback) {
-            if (callback.is_error()) {
-                std::cerr << "Failed to send message";
-            }
-        });
     }
 }
 
@@ -65,8 +46,8 @@ int main() {
     bot.on_ready([&bot](const dpp::ready_t& event) {
         std::cout << "Bot is online!" << std::endl;
 
-        // Start the periodic message sending in a separate thread
-        std::thread messageThread(send_periodic_message, std::ref(bot), CHANNEL_ID);
+        // Start the FIFO  message sending in a separate thread
+        std::thread messageThread(send_message_from_fifo, std::ref(bot), CHANNEL_ID);
         messageThread.detach(); // Detach the thread to allow it to run independently
     });
 
